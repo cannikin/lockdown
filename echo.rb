3855eca3e596181e3580aad65e77e28314ded368
Rob Cameron <cannikinn@gmail.com>
2014-02-11 18:25:50 -0800

    Modifies sample echo to talk to Guarddog

--- echo.rb
+++ echo.rb
@@ -1,8 +1,12 @@
 require 'sinatra'
 require 'sinatra-websocket'
+require './guarddog'
+require 'pry'
+require 'pry-nav'
 
 set :server, 'thin'
 set :sockets, []
+set :guarddog, Guarddog.new('/dev/tty.usbmodem40111')
 
 get '/' do
   if !request.websocket?
@@ -12,13 +16,17 @@ get '/' do
       ws.onopen do
         ws.send("Hello World!")
         settings.sockets << ws
+        EM.next_tick { settings.sockets.each { |s| s.send(settings.guarddog.status.to_json) } }
       end
       ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        EM.next_tick { settings.sockets.each { |s| s.send(settings.guarddog.poll.to_json) } }
       end
       ws.onclose do
         warn("websocket closed")
         settings.sockets.delete(ws)
+        if settings.sockets.empty?
+          settings.guarddog.close
+        end
       end
     end
   end
