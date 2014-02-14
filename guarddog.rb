6780183840f136a20c5864a5b2eccf0f66eb938e
Rob Cameron <cannikinn@gmail.com>
2014-02-13 22:46:12 -0800

    Moves auxiliary methods to method_missing

--- guarddog.rb
+++ guarddog.rb
@@ -1,79 +1,71 @@
-# Talks to Arduino and reports status updates
-require 'serialport'
+# Talks to Arduino speaking the Lockdown protocol
 
+require 'serialport'
+require 'json'
 # lets us query serial.ready? to see if there are any bytes waiting for us or
 # serial.nread to tell us how many bytes are available
 require 'io/wait'
-require 'json'
 
 class Guarddog
 
-  MAX_RETRIES = 3
+  class PortRequiredError < StandardError
+    def initialize(msg="A USB port is required to connect to. e.g. '/dev/tty.usbmodem14001'")
+      super(msg)
+    end
+  end
+
+  METHODS = [:status, :version]
 
   attr_reader :serial, :port
 
-  def initialize(port='/dev/tty.usbmodem40111')
+
+  def initialize(port=nil)
+    raise PortRequiredError if port.nil?
     @port = port
-    connect
   end
 
+
   # connect to the serial port
-  def connect
+  def connect!
     @serial = SerialPort.new(port, 57600, 8, 1, SerialPort::NONE)
   end
+
 
   # will either return an empty array or all events since the last check
   def poll
     output = []
     retries = 0
     last = ''
-    begin
-      while serial.ready?
-        last = serial.gets.chomp
-        output << JSON.parse(last) if validate(last)
-      end
-      return output
-    # rescue IOError => e
-    #   # Reconnect
-    #   retries += 1
-    #   if retries < MAX_RETRIES
-    #     connect
-    #     retry
-    #   else
-    #     raise e
-    #   end
-    # rescue JSON::ParserError => e
-    #   puts "Bad JSON: #{last}"
-    #   return []
+    while serial.ready?
+      last = serial.gets.chomp
+      output << JSON.parse(last) if validate(last)
     end
+    return output
   end
 
-  def status
-    begin
-      write({ :message => 'status' })
-    # rescue IOError => e
-    #   # Reconnect
-    #   retries += 1
-    #   if retries < MAX_RETRIES
-    #     connect
-    #     retry
-    #   else
-    #     raise e
-    #   end
-    end
-    sleep(0.5)
-    poll
-  end
 
   # close the connection
-  def close
+  def close!
     serial.close
+    puts "Serial closed."
+  end
+
+
+  # Pass other method calls over to the Arduino
+  def method_missing(method_name, *args)
+    if METHODS.include? method_name
+      write({ :method => method_name })
+      poll
+    else
+      super
+    end
   end
 
 private
 
   def write(message)
     serial.puts(message.to_json)
+    sleep(0.5) # TODO: make this async
   end
 
   # Validate a message from Arduino
