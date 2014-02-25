b3976810928b6ab80f9ced4b9845d0b9a5550031
Rob Cameron <cannikinn@gmail.com>
2014-02-24 23:30:59 -0800

    Log web socket events

--- lib/web_socket_parser.rb
+++ lib/web_socket_parser.rb
@@ -2,8 +2,10 @@
 
 class WebSocketParser
 
-  def parse(raw_message)
+  def parse(raw_message, options={})
+    options = { :log => true }.merge(options)
     message = JSON.parse(raw_message)
+    log_event(message) if options[:log]
     output = []
 
     case message['event']
@@ -12,6 +14,14 @@ class WebSocketParser
       output << { :event => 'change_mode', :data => { :mode => message['data']['mode'] }}
     end
     return output
+  end
+
+private
+
+  def log_event(message)
+    Event.create :type => message['event'],
+                 :data => message.to_json,
+                 :created_at => Time.now
   end
 
 end
