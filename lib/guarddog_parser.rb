a81864d8027093a986fb1bd1224e6e17d7c2775b
Rob Cameron <cannikinn@gmail.com>
2014-02-24 23:30:48 -0800

    Refactors Guarddog event logging

--- lib/guarddog_parser.rb
+++ lib/guarddog_parser.rb
@@ -5,9 +5,11 @@ class GuarddogParser
   def parse(events, options={})
     options = { :log => true }.merge(options)
     output = []
+
     if events.any?
       log_events(events) if options[:log]
       events.each do |event|
+        update_sensor(event)
         case event['result']['type']
         when 'egress'
           output << egress_state_change(event)
@@ -21,14 +23,15 @@ private
 
   def log_events(events)
     events.each do |event|
-      Event.create :arduino_id => event['result']['id'],
-                   :type       => event['result']['type'],
-                   :value      => event['result']['value'],
-                   :millis     => event['result']['millis'],
+      Event.create :type => event['result']['type'],
+                   :data => events['result'].to_json,
                    :created_at => Time.now
-      if sensor = Sensor.where(:arduino_id => event['result']['id'].to_i).first
-        sensor.update(:value => event['result']['value'], :updated_at => Time.now)
-      end
+    end
+  end
+
+  def update_sensor(event)
+    if sensor = Sensor.where(:arduino_id => event['result']['id'].to_i).first
+      sensor.update(:value => event['result']['value'], :updated_at => Time.now)
     end
   end
 
